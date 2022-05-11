6d4560045ede839c9780e56c6690944cee06c7a7
AnqiaoLi <3170104825@zju.edu.cn>
2022-05-11 11:38:16 +0200

    revise vismodel to 4d channels input

--- adabins/vismodel.py
+++ adabins/vismodel.py
@@ -4,18 +4,18 @@
 from train import *
 import matplotlib.pyplot as plt
 args = parse_args("@args_train.txt")
-args.data_path = "/media/anqiao/Semantic/Data/extract_trajectories_001/"
+args.data_path = "/media/anqiao/Semantic/Data/extract_trajectories/"
 
 test_loader = DepthDataLoader(args, 'online_eval').data
 model = models.UnetAdaptiveBins.build(n_bins=args.n_bins, min_val=args.min_depth, max_val=args.max_depth,
                                           norm=args.norm)
-model,opt,epoch = model_io.load_checkpoint("./checkpoints/AdaBins_kitti.pt" ,model)
+model,opt,epoch = model_io.load_checkpoint("./checkpoints/UnetAdaptiveBins_09-May_22-30-nodebs6-tep25-lr0.000357-wd0.1-920d65a0-231f-4df2-a415-056d6a051ea6_best.pt" ,model)
 
 
 sample = next(iter(test_loader))
 
 
-inputimg = np.moveaxis(sample["image"][0].numpy(),0,2)
+inputimg = np.moveaxis(sample["image"][0][0:3, :, :].numpy(),0,2)
 inputimg.max(), inputimg.min()
 inputimg = (inputimg-inputimg.min())/(inputimg.max()- inputimg.min())
 fig, axs = plt.subplots(1, 3,figsize=(20, 6))
