b7b8cebcd44517d5671e0cf996a5059bc2ace9db
Chenyu Yang <chenyang@student.ethz.ch>
2022-05-25 17:13:00 +0200

    add ros node mode in rosvis

--- adabins/rosvis.py
+++ adabins/rosvis.py
@@ -29,21 +29,95 @@ m.patch()
 import rospy
 from sensor_msgs import point_cloud2
 from sensor_msgs.msg import PointCloud2, PointField
+from sensor_msgs.msg import Image
 from std_msgs.msg import Header
+from std_msgs.msg import Float64MultiArray
 import rosgraph
 assert rosgraph.is_master_online()
+from threading import Lock
 
 import sys
 sys.path.append("../Labelling/")
 from messages.imageMessage import Camera
+from scipy.spatial.transform import Rotation
 from train import *
 
+
+def calculate_H_map_cam(transition, rotation):
+    
+    H_map_cam = np.eye(4)
+    H_map_cam[:3,3] =  np.array( [transition])
+    H_map_cam[:3,:3] = Rotation.from_euler('zyx', [[-math.pi-rotation[2], rotation[1], rotation[0]]]).as_matrix() # looking down
+    #         H_map_cam[:3,:3] = Rotation.from_euler('zyx', [[-np.math.pi-rotation[2], rotation[1], rotation[0]]], degrees=False).as_matrix() # looking down
+
+    H_map_cam[:3,:3] = Rotation.from_euler('yz', [0, 180], degrees=True).as_matrix() @ H_map_cam[:3,:3]
+    return H_map_cam
+
+
 class RosVisulizer:
-    def __init__(self, topic):
+    def __init__(self, topic, camera_calibration_path="/ros/catkin_ws/src/mnt/darpa_subt/anymal/anymal_chimera_subt/anymal_chimera_subt/config/calibrations/alphasense/"):
         self.pub = rospy.Publisher(topic, PointCloud2, queue_size=1)
         # if(rospy.)
         rospy.init_node('ros_visulizer', anonymous=True)
-        
+        self.camera_calibration_path = camera_calibration_path
+        self.init_camera()
+    
+    def init_camera(self):
+        camera_calibration_path = self.camera_calibration_path
+        cam_id = "cam4"
+        cfg={}
+        cfg["CAM_RBSWAP"]=['']
+        cfg["CAM_SUFFIX"]= '/dropped/debayered/compressed'
+        cfg["CAM_PREFIX"]= '/alphasense_driver_ros/'
+        self.camera = Camera(camera_calibration_path, cam_id, cfg)
+        self.camera.tf_base_to_sensor = (np.array([-0.40548693, -0.00076062,  0.23253198]), 
+                        np.array([[-0.00603566,  0.00181943, -0.99998013,  0.        ],
+                            [ 0.99997436,  0.00386421, -0.00602859,  0.        ],
+                            [ 0.00385317, -0.99999088, -0.00184271,  0.        ],
+                            [ 0.        ,  0.        ,  0.        ,  1.        ]]))
+        W,H = self.camera.image_width,self.camera.image_height
+        pixel_cor = np.mgrid[0:W,0:H]
+        pixel_cor_hom = np.concatenate( [ pixel_cor, np.ones_like(pixel_cor[None,0,:,:])], axis=0 )
+        self.pixel_cor_hom = pixel_cor_hom
+
+    
+    def build_could_from_depth_image(self, pose, depth, image):
+        """
+        depth: a torch tensor depth image
+        image: the color image, can be numpy or torch
+        """
+        self.camera.update_pose_from_base_pose(pose)
+        K = self.camera.camera_matrix
+        W,H = self.camera.image_width,self.camera.image_height
+        ray_dir = (np.linalg.inv(K) @ (self.pixel_cor_hom.reshape(3,-1))).T
+        ray_dir = ray_dir/ np.linalg.norm(ray_dir, axis=1)[:,None]
+
+        position = self.camera.pose[0]
+        euler = euler_from_matrix(self.camera.pose[1][:3,:3])
+            
+        H_map_cam = calculate_H_map_cam(position, euler)
+        R = torch.from_numpy( H_map_cam )[:3,:3]
+        directions = torch.from_numpy(ray_dir )
+        directions = (directions @ R)
+        start_points = torch.from_numpy( H_map_cam[:3,3])
+        pts = start_points + depth.reshape(-1,1)*directions
+
+        header = Header()
+        header.frame_id = "map"
+
+        fields = [
+            PointField('x', 0, PointField.FLOAT32, 1),
+            PointField('y', 4, PointField.FLOAT32, 1),
+            PointField('z', 8, PointField.FLOAT32, 1),
+            # PointField('rgb', 12, PointField.UINT32, 1),
+            PointField('rgba', 12, PointField.UINT32, 1),
+        ]
+
+        im_color = image.reshape(-1,3)
+        cloud = point_cloud2.create_cloud(header, fields, 
+                    [rosv.buildPoint(*p[:3], *c,a=0.2) for p,c in zip(pts, im_color)])
+        return cloud
+
     def buildPoint(self, x,y,z,r,g,b,a=None):
         if(np.array([r,g,b]).max()<1.01):
             r = int(r * 255.0)
@@ -118,73 +192,11 @@ def vis_from_dataset(sample):
     print("POS:", data['pointcloud'][:,:3].mean(axis = 0))
 
     ## Broad cast depth image
-    depthimg = sample["depth"]
-    camera_calibration_path = "/ros/catkin_ws/src/mnt/darpa_subt/anymal/anymal_chimera_subt/anymal_chimera_subt/config/calibrations/alphasense/"
-    cam_id = "cam4"
-    cfg={}
-    cfg["CAM_RBSWAP"]=['']
-    cfg["CAM_SUFFIX"]= '/dropped/debayered/compressed'
-    cfg["CAM_PREFIX"]= '/alphasense_driver_ros/'
-    camera = Camera(camera_calibration_path, cam_id, cfg)
-    camera.tf_base_to_sensor = (np.array([-0.40548693, -0.00076062,  0.23253198]), 
-                    np.array([[-0.00603566,  0.00181943, -0.99998013,  0.        ],
-                        [ 0.99997436,  0.00386421, -0.00602859,  0.        ],
-                        [ 0.00385317, -0.99999088, -0.00184271,  0.        ],
-                        [ 0.        ,  0.        ,  0.        ,  1.        ]]))
-    camera.update_pose_from_base_pose(data["pose"]["map"])
-    K = camera.camera_matrix
-    W,H = camera.image_width,camera.image_height
-    pixel_cor = np.mgrid[0:W,0:H]
-    pixel_cor_hom = np.concatenate( [ pixel_cor, np.ones_like(pixel_cor[None,0,:,:])], axis=0 )
-
-    ray_dir = (np.linalg.inv(K) @ (pixel_cor_hom.reshape(3,-1))).T
-    ray_dir = ray_dir/ np.linalg.norm(ray_dir, axis=1)[:,None]
-
-
-    def calculate_H_map_cam(transition, rotation):
-        from scipy.spatial.transform import Rotation
-        H_map_cam = np.eye(4)
-        H_map_cam[:3,3] =  np.array( [transition])
-        H_map_cam[:3,:3] = Rotation.from_euler('zyx', [[-math.pi-rotation[2], rotation[1], rotation[0]]]).as_matrix() # looking down
-        #         H_map_cam[:3,:3] = Rotation.from_euler('zyx', [[-np.math.pi-rotation[2], rotation[1], rotation[0]]], degrees=False).as_matrix() # looking down
-
-        H_map_cam[:3,:3] = Rotation.from_euler('yz', [0, 180], degrees=True).as_matrix() @ H_map_cam[:3,:3]
-        return H_map_cam
-
-    # position = np.array(data["pose"]["map"][:3])
-    # euler = np.array(euler_from_quaternion(data["pose"]["map"][3:]))
-    position = camera.pose[0]
-    euler = euler_from_matrix(camera.pose[1][:3,:3])
-        
-    H_map_cam = calculate_H_map_cam(position, euler)
-    R = torch.from_numpy( H_map_cam )[:3,:3]
-    directions = torch.from_numpy(ray_dir )
-    directions = (directions @ R)
-    start_points = torch.from_numpy( H_map_cam[:3,3])
     depth = sample["depth"][0][0].T
-    pts = start_points + depth.reshape(-1,1)*directions
 
-    header = Header()
-    header.frame_id = "map"
-
-    fields = [
-        PointField('x', 0, PointField.FLOAT32, 1),
-        PointField('y', 4, PointField.FLOAT32, 1),
-        PointField('z', 8, PointField.FLOAT32, 1),
-        # PointField('rgb', 12, PointField.UINT32, 1),
-        PointField('rgba', 12, PointField.UINT32, 1),
-    ]
-
-    pts_color = pts[None,:,2,None]
-    pts_color = ((pts_color-pts_color.min())/(pts_color.max()-pts_color.min())*255).numpy().astype(np.uint8)
-    # im_color = cv2.applyColorMap(pts_color, cv2.COLORMAP_OCEAN)[0]
-    image = np.transpose(sample["image"][0,:3,...],(2,1,0)).reshape(-1,3)
+    image = np.transpose(sample["image"][0,:3,...],(2,1,0))
     image = (image-image.min())/(image.max()-image.min())
-    im_color = image
-    # colors = (pts[:,2,None] - pts[:,2].min())/(pts[:,2].max() - pts[:,2].min()) * np.ones([1,3])
-    cloud = point_cloud2.create_cloud(header, fields, 
-                [rosv.buildPoint(*p[:3], *c,a=0.2) for p,c in zip(pts, im_color)])
-
+    cloud = rosv.build_could_from_depth_image(data["pose"]["map"], depth, image)
     print("terrainpub", cloud.height, cloud.width)
     terrainpub.publish(cloud)
 
@@ -197,41 +209,153 @@ def vis_from_dataset(sample):
 
     _, pred = model(sample["image"])
     pred = pred[0].detach().numpy()
-    pred = nn.functional.interpolate(torch.tensor(pred)[None,...], torch.tensor(depth.T).shape[-2:], mode='bilinear', align_corners=True)
-    # pred = pred.reshape(-1)
+    pred = nn.functional.interpolate(torch.tensor(pred).detach()[None,...], torch.tensor(depth.T).shape[-2:], mode='bilinear', align_corners=True)
+    pred = pred[0][0].T
+    pred_color = ((pred-pred.min())/(pred.max()-pred.min())*255).numpy().astype(np.uint8)
+    im_color = cv2.applyColorMap(pred_color, cv2.COLORMAP_OCEAN)
 
-    pred_pts = start_points + pred[0][0].T.reshape(-1,1)*directions
-
-    pts_color = ((pts_color-pts_color.min())/(pts_color.max()-pts_color.min())*255).astype(np.uint8)
-    im_color = cv2.applyColorMap(pts_color, cv2.COLORMAP_OCEAN)[0]
-    # colors = (pts[:,2,None] - pts[:,2].min())/(pts[:,2].max() - pts[:,2].min()) * np.ones([1,3])
-    cloud = point_cloud2.create_cloud(header, fields, 
-                [rosv.buildPoint(*p[:3], *c, a=0.5) for p,c in zip(pred_pts, im_color)])
-
-
+    cloud = rosv.build_could_from_depth_image(data["pose"]["map"], pred, im_color)
     predpub.publish(cloud)
 
 if __name__ == "__main__":
 
+    MODE = "MODE_NODE"
     rosv = RosVisulizer("pointcloud")
-    
     parser.add_argument("--model", default="")
     args = parse_args()
-    # checkpoint_path = args.models
-    # model = models.UnetAdaptiveBins.build(n_bins=args.modelconfig.n_bins, min_val=args.min_depth, max_val=args.max_depth,
-    #                                         norm=args.modelconfig.norm)
-    # model,_,_ = model_io.load_checkpoint(checkpoint_path ,model) 
+    if(MODE=="MODE_DATASET"):
+        # checkpoint_path = args.models
+        # model = models.UnetAdaptiveBins.build(n_bins=args.modelconfig.n_bins, min_val=args.min_depth, max_val=args.max_depth,
+        #                                         norm=args.modelconfig.norm)
+        # model,_,_ = model_io.load_checkpoint(checkpoint_path ,model) 
 
-    ## Loader
-    args.data_path = "/Data/extract_trajectories_002"
-    ## Loader
-    data_loader = DepthDataLoader(args, 'online_eval').data
-    # sample = next(iter(data_loader))
-    data_iter = iter(data_loader)
+        ## Loader
+        args.data_path = "/Data/extract_trajectories_002"
+        ## Loader
+        data_loader = DepthDataLoader(args, 'online_eval').data
+        # sample = next(iter(data_loader))
+        data_iter = iter(data_loader)
+        sample = next(data_iter)
+        vis_from_dataset(sample)
 
-    sample = next(data_iter)
+    elif(MODE=="MODE_NODE"):
+        checkpoint_path = "checkpoints/share/2022-05-14-00-19-41/UnetAdaptiveBins_best.pt"
+        model = models.UnetAdaptiveBins.build(n_bins=args.modelconfig.n_bins, min_val=args.min_depth, max_val=args.max_depth,
+                                                norm=args.modelconfig.norm)
+        model,_,_ = model_io.load_checkpoint(checkpoint_path ,model) 
 
+        # the place holders for the values
+        pose_ph = None
+        depth_ph = None
+        image_ph = None
 
-    vis_from_dataset(sample)
+        pose_mutex = Lock()
+        depth_mutex = Lock()
+        image_mutex = Lock()
+
+        show_pred_flag = True
+        show_depth_flag = True
+
+        def pose_callback(data):
+            global pose_ph
+            pose_mutex.acquire()
+            try:
+                pose_ph = torch.tensor(data.data)
+                print("received pose", pose_ph)
+            finally:
+                pose_mutex.release()
+
+        def depth_callback(data):
+            global depth_ph
+            depth_mutex.acquire()
+            try:
+                print(data.layout.dim)
+                shape = [d.size for d in data.layout.dim]
+                depth_ph = torch.tensor(data.data).reshape(shape)
+                print("received depth image", depth_ph.shape)
+            finally:
+                depth_mutex.release()
     
+        def image_callback(data):
+            global image_ph
+            image_mutex.acquire()
+            try:
+                print(data.layout.dim)
+                shape = [d.size for d in data.layout.dim]
+                image_ph = torch.tensor(data.data).reshape(shape)
+                print("received image image", image_ph.shape)
+            finally:
+                image_mutex.release()
 
+        rospy.Subscriber("semantic_filter_pose", Float64MultiArray, pose_callback)
+        rospy.Subscriber("semantic_filter_depth", Float64MultiArray, depth_callback)
+        rospy.Subscriber("semantic_filter_image", Float64MultiArray, image_callback)
+
+        rate = rospy.Rate(0.1) # 1hz
+        while not rospy.is_shutdown():
+            rate.sleep()
+            image_mutex.acquire()
+            image = image_ph.clone() if image_ph is not None else image_ph
+            image_mutex.release()
+            depth_mutex.acquire()
+            depth = depth_ph.clone() if depth_ph is not None else depth_ph
+            depth_mutex.release()
+            pose_mutex.acquire()
+            pose = pose_ph.clone() if pose_ph is not None else pose_ph
+            pose_mutex.release()
+            
+            if(show_depth_flag and depth is not None and image is not None and pose is not None):
+                _depth = depth.T
+                _image = np.transpose(image.numpy(),(2,1,0))
+                _image = (_image-_image.min())/(_image.max()-_image.min())
+                cloud = rosv.build_could_from_depth_image(pose, _depth, _image)
+                print("terrainpub", cloud.height, cloud.width)
+                terrainpub.publish(cloud)
+
+            if(show_pred_flag and image is not None and pose is not None):
+                _image = torch.cat([image, torch.zeros_like(image[:1,...])], axis = 0) # add the pc channel
+                _image = _image[None,...]
+                _, pred = model(_image)
+                print("pred",pred.shape)
+                pred = pred[0].detach().numpy()
+                pred = nn.functional.interpolate(torch.tensor(pred).detach()[None,...], _image.shape[-2:], mode='bilinear', align_corners=True)
+                pred = pred[0][0].T
+                pred_color = ((pred-pred.min())/(pred.max()-pred.min())*255).numpy().astype(np.uint8)
+                im_color = cv2.applyColorMap(pred_color, cv2.COLORMAP_OCEAN)
+
+                cloud = rosv.build_could_from_depth_image(pose, pred, im_color)
+                predpub.publish(cloud)
+
+"""
+This ros node mode can work with a publisher as the following
+
+```python
+import rospy
+from std_msgs.msg import Header
+from std_msgs.msg import Float64MultiArray, MultiArrayDimension
+import msgpack
+import msgpack_numpy as m
+import numpy as np
+m.patch()
+rospy.init_node('dump_publisher', anonymous=True)
+pose_pub = rospy.Publisher("/semantic_filter_pose", Float64MultiArray, queue_size=1)
+image_pub = rospy.Publisher("/semantic_filter_image", Float64MultiArray, queue_size=1)
+depth_pub = rospy.Publisher("/semantic_filter_depth", Float64MultiArray, queue_size=1)
+
+with open("/Data/extract_trajectories_002/WithPointCloudReconstruct_2022-03-26-22-28-54_0/traj_2_datum_4.msgpack", "rb") as data_file:
+    byte_data = data_file.read()
+    data = msgpack.unpackb(byte_data)
+
+pose_pub.publish(Float64MultiArray(data = data["pose"]['map']))
+
+img = data["images"]["cam4"].astype(np.float64)
+img_msg = Float64MultiArray(data = img.reshape(-1))
+img_msg.layout.dim=[MultiArrayDimension(size=d) for d in img.shape]
+image_pub.publish(img_msg)
+
+img = data["images"]["cam4depth"].astype(np.float64)[:1,...]
+img_msg = Float64MultiArray(data = img.reshape(-1))
+img_msg.layout.dim=[MultiArrayDimension(size=d) for d in img.shape]
+depth_pub.publish(img_msg)
+```
+"""
