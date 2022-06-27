42fad72c8e1386e010481dd8b6ca8534642110d5
Anqiao Li <anqiali@eu-login-32.euler.ethz.ch>
2022-06-27 22:49:44 +0200

    training with inpaint has denser prediction

--- adabins/cfg.py
+++ adabins/cfg.py
@@ -93,7 +93,7 @@ class TrainConfig:
     dataset: str = "anymal"
     slim_dataset: bool = True # whether or not the dataset is slimed version: (contain projected pc instead of full point cloud information)
     pc_img_input_channel: int = 0 # Which channel of the point cloud image to use, the pc imges have different level of augmentation (slim_dataset is needed)
-    pc_img_label_channel: int = 1 # Which channel of the point cloud image to use, the pc imges have different level of augmentation (slim_dataset is needed)
+    pc_img_label_channel: int = 0 # Which channel of the point cloud image to use, the pc imges have different level of augmentation (slim_dataset is needed)
 
     lr: float =  0.000357
     wd: float =  0.1
--- adabins/loss.py
+++ adabins/loss.py
@@ -47,8 +47,8 @@ class UncertaintyLoss(nn.Module):  # Add variance to loss
         # Dg = torch.var(g) + 0.15 * torch.pow(torch.mean(g), 2)
         if(target_variance.numel() !=0):
             # Dg = 1/(input.shape[0]) * torch.sum(0.5 * torch.pow(input - target, 2)/target_variance)
-            Dg = torch.sum(torch.pow(input - target, 2))
-            # Dg = 1/(input.shape[0]) * torch.sum(0.5 * torch.pow(input - target, 2)/(target/40.0 + target_variance))
+            #Dg = torch.sum(torch.pow(input - target, 2))
+            Dg = 1/(input.shape[0]) * torch.sum(0.5 * torch.pow(input - target, 2)/(target/40.0 + target_variance))
             if(self.args.scale_loss_with_point_number):
                 Dg /= input.shape[0]
         else:
