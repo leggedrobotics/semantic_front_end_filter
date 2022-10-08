883953f65829085126b0ad6de46f9315ae4bb407
Chenyu Yang <chenyang@student.ethz.ch>
2022-10-08 11:39:12 +0200

    change for convinence

--- semantic_front_end_filter/adabins/models/unet_adaptive_bins.py
+++ semantic_front_end_filter/adabins/models/unet_adaptive_bins.py
@@ -25,6 +25,7 @@ class UpSampleBN(nn.Module):
 
     def forward(self, x, concat_with):
         up_x = F.interpolate(x, size=[concat_with.size(2), concat_with.size(3)], mode='nearest')
+        # up_x = F.interpolate(x, size=[concat_with.size(2), concat_with.size(3)], mode='bilinear', align_corners=True)
         f = torch.cat([up_x, concat_with], dim=1)
         return self._net(f)
 
@@ -64,6 +65,7 @@ class DecoderBN(nn.Module):
             # x_d5 = self.up5_add(x_d4, x_block_skip[:, 3, :, :])
             x_d5 = self.conv3(x_d4)
             out = x_block_skip[:, 3:, :, :] +  self.distance_maintainer(F.interpolate(x_d5, size=[x_block_skip.size(2), x_block_skip.size(3)], mode='nearest'))
+            # out = x_block_skip[:, 3:, :, :] + F.interpolate(x_d5, size=[x_block_skip.size(2), x_block_skip.size(3)], mode='bilinear', align_corners=True)
         else:
             out = self.conv3(x_d4)
         # out = self.act_out(out)
