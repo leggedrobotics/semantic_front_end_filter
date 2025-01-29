5c3ccb2e3995005318514e44c479676b96488cdb
AnqiaoLi <3170104825@zju.edu.cn>
2025-01-29 14:02:29 +0100

    change the IP claim

--- semantic_front_end_filter/models/__init__.py
+++ semantic_front_end_filter/models/__init__.py
@@ -1,17 +1,17 @@
-# This file is part of MyProject.
+# This file is part of SemanticFrontEndFilter.
 #
-# MyProject is free software: you can redistribute it and/or modify
+# SemanticFrontEndFilter is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# MyProject is distributed in the hope that it will be useful,
+# SemanticFrontEndFilter is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with MyProject.  If not, see <https://www.gnu.org/licenses/>.
+# along with SemanticFrontEndFilter.  If not, see <https://www.gnu.org/licenses/>.
 
 
 from .unet_adaptive_bins import UnetAdaptiveBins
